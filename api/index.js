4ae49a818ca31c6d77336a1e0667d71b4e4f406d
mancorago-web <mancora.go@gmail.com>
2026-08-05 22:33:08 -0500

    fix: HTML sin cache (Cache-Control no-store) + app.js v=17 para que COMPRAS cargue siempre la version nueva

--- api/index.js
+++ api/index.js
@@ -216,6 +216,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));
 
 app.get('/', (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.render('index');
 });
 
